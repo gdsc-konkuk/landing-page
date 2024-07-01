e0a1158258278c3abfc5f5ac0c5ff3a325e3adf5
MinboyKim <kdm3801@gmail.com>
2024-07-02 05:23:45 +0900

    feat: Header
    Header implementation

--- app/_component/Header.tsx
+++ app/_component/Header.tsx
@@ -1,9 +1,21 @@
+import Image from 'next/image';
+
 export default function Header() {
   return (
-    <div>
-      <h1>Pretendard</h1>
-      <h1 className="font-suite">Suite</h1>
-      <h1 className="font-google">Google Sans</h1>
+    <div className="w-full h-[66px] pl-[54px] pt-[19px] pb-[18px]">
+      <div className="flex items-center">
+        <Image
+          src="/images/header/header-symbol.png"
+          alt="header-symbol"
+          width={45}
+          height={21}
+          className="w-[45px] h-[21px]"
+        />
+        <h1 className="font-google font-bold text-[23px] leading-normal ml-[12px] mr-[8px]">
+          GDSC
+        </h1>
+        <span className="text-[16px] text-[#7C7C7C]">Konkuk Univ.</span>
+      </div>
     </div>
   );
 }
