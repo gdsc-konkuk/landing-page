3e03a4920eae1f6a789f395d9956db60f59983d3
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-07-19 15:01:49 +0900

    fix: delete Review section in page.tsx
    -trying to pull main code

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
 import Footer from './_component/Footer';
 import Header from './_component/Header';
 import Main from './_component/main/Main';
-import Review from './_component/review/Review';
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <Header />
       <Main />
-      <Review />
       <Footer />
     </div>
   );
