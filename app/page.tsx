57341d60d7eea381135444e3cf3b01d4ac5128e3
clice lee <cliceleee@gmail.com>
2024-07-20 21:57:45 +0900

    Merge main

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Footer from './_component/Footer';
 import Header from './_component/Header';
 import Main from './_component/main/Main';
 import SolutionChallenge from './_component/solution-challenge/SolutionChallenge';
+import Review from './_component/review/Review';
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <Header />
       <Main />
+      <Review />
       <SolutionChallenge />
       <Footer />
     </div>
