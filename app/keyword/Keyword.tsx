c7ee8dec63b7e42c08573a208e189ce5e3bd7952
clice lee <cliceleee@gmail.com>
2024-07-28 02:02:17 +0900

    design: import keyword card images

--- app/keyword/Keyword.tsx
+++ app/keyword/Keyword.tsx
@@ -4,6 +4,10 @@ import React, { useState } from 'react';
 import Card from './Card';
 import Dot from './Dot';
 
+import keywordTogether from '../../public/images/keyword/keywordTogether.png'
+import keywordSharing from '../../public/images/keyword/keywordSharing.png'
+import keywordChallenge from '../../public/images/keyword/keywordChallenge.png'
+
 interface CardData {
     title: string;
     description: string;
@@ -12,19 +16,19 @@ interface CardData {
 
 const cards: CardData[] = [
   {
+    image: keywordChallenge,
     title: "Challenge",
     description: "GDSC Konkuk에서 열정을 가지고 도전하며 발전할 수 있습니다.",
-    image: "path/to/challenge-image.png",
   },
   {
+    image: keywordSharing,
     title: "Sharing",
     description: "GDSC Konkuk은 공유를 통해 서로의 지식을 나누고 함께 성장하는 커뮤니티 입니다.",
-    image: "path/to/sharing-image.png",
   },
   {
+    image: keywordTogether,
     title: "Together",
     description: "GDSC Konkuk에서는 혼자 얻을 수 없는 소중한 가치를 함께 창출할 수 있습니다.",
-    image: "path/to/together-image.png",
   }
 ];
 
