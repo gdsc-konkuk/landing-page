493afc79952c64c152c51282812f7d870e9389a0
clice lee <cliceleee@gmail.com>
2024-07-28 03:08:36 +0900

    design: adjust CardData properties - use flex and flex-col

--- app/keyword/Card.tsx
+++ app/keyword/Card.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import Image from 'next/image';
 
 interface CardProps {
   card: {
+    subtitle: string;
     title: string;
     description: string;
     image: string;
@@ -10,12 +12,16 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ card }) => {
   return (
-    <div className="bg-white rounded-lg shadow-lg p-6 max-w-lg">
-      <img src={card.image} alt={card.title} className="rounded-t-lg mb-4" />
-      <h2 className="text-2xl font-bold mb-2">{card.title}</h2>
-      <p>{card.description}</p>
+    <div className='flex items-center'>
+      <div className='flex-col'>
+        <div className="text-[#ea4335] text-[22px] font-semibold font-suite leading-9">{card.subtitle}</div>
+        <h2 className="text-5xl font-bold mt-[12px] relative">{card.title}</h2>
+        <div className='w-[306px] h-[120px] text-[#606060] text-[28px] font-normal font-suite leading-10 mt-[23.12px]'>{card.description}</div>
+      </div>
+      <Image src={card.image} alt={card.title} className="w-[714px] h-[574.12px] relative ml-[102px]" />
     </div>
   );
 }
 
 export default Card;
+
--- app/keyword/Keyword.tsx
+++ app/keyword/Keyword.tsx
@@ -9,26 +9,32 @@ import keywordSharing from '../../public/images/keyword/keywordSharing.png'
 import keywordChallenge from '../../public/images/keyword/keywordChallenge.png'
 
 interface CardData {
+    subtitle: string;
     title: string;
     description: string;
     image: string;
 }
 
 const cards: CardData[] = [
+
+
   {
-    image: keywordChallenge,
-    title: "Challenge",
-    description: "GDSC Konkuk에서 열정을 가지고 도전하며 발전할 수 있습니다.",
+    image: keywordTogether,
+    subtitle: "함께",
+    title: "Together",
+    description: "GDSC Konkuk에서는 혼자 얻을 수 없는 소중한 가치를 함께 창출할 수 있습니다.",
   },
   {
     image: keywordSharing,
+    subtitle: "공유",
     title: "Sharing",
     description: "GDSC Konkuk은 공유를 통해 서로의 지식을 나누고 함께 성장하는 커뮤니티 입니다.",
   },
   {
-    image: keywordTogether,
-    title: "Together",
-    description: "GDSC Konkuk에서는 혼자 얻을 수 없는 소중한 가치를 함께 창출할 수 있습니다.",
+    image: keywordChallenge,
+    subtitle: "도전",
+    title: "Challenge",
+    description: "GDSC Konkuk에서 열정을 가지고 도전하며 발전할 수 있습니다.",
   }
 ];
 
@@ -36,9 +42,9 @@ const Keyword: React.FC = () => {
   const [activeIndex, setActiveIndex] = useState(0);
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="flex flex-col items-center">
-        <div className="flex space-x-4 mb-8">
+    <div className="h-screen w-screen flex justify-center bg-white-100">
+      <div className="flex">
+        <div className="">
           {cards.map((_, index) => (
             <Dot key={index} onHover={() => setActiveIndex(index)} />
           ))}
