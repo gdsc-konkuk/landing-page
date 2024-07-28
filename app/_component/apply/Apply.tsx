3de2e4fef8c3a32d610a629d38ccf7675805f2b6
clice lee <cliceleee@gmail.com>
2024-07-28 17:17:39 +0900

    feat: add raw code in Apply section

---
+++ app/_component/apply/Apply.tsx
@@ -0,0 +1,14 @@
+export default function Main() {
+  return (
+    <div className="flex flex-col items-center text-center">
+        <h1 className='text-black text-[64px] font-bold'>GDSC Konkuk 2기와 </h1>
+        <div className='text-black text-3xl font-semibold mt-[40px] space-y-[8px]'>
+            <p>GDSC와 함께 가치를 공유하고 성장하고 싶다면,</p>
+            <p>2024.08.01.~2025.08.01</p>
+        </div>
+    </div>
+  );
+}
+
+
+
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Main from './_component/main/Main';
 import SolutionChallenge from './_component/solution-challenge/SolutionChallenge';
 import Review from './_component/review/Review';
 
+import Apply from './_component/apply/Apply';
 export default function Home() {
   return (
     <div className="flex flex-col">
@@ -11,6 +12,7 @@ export default function Home() {
       <Main />
       <Review />
       <SolutionChallenge />
+      <Apply />
       <Footer />
     </div>
   );
