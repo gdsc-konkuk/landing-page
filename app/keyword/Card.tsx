a63082f9204fde7445f96d9f3b7379eccb8aa183
clice lee <cliceleee@gmail.com>
2024-08-01 23:54:28 +0900

    add: useEffect to control Opacity

--- app/keyword/Card.tsx
+++ app/keyword/Card.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
 import Image from 'next/image';
 
 interface CardProps {
@@ -11,14 +11,27 @@ interface CardProps {
 }
 
 const Card: React.FC<CardProps> = ({ card }) => {
+  const [opacity, setOpacity] = useState(1);
+  const [currentCard, setCurrentCard] = useState(card);
+
+  useEffect(() => {
+    if (card !== currentCard) {
+      setOpacity(0);
+      const timer = setTimeout(() => {
+        setCurrentCard(card);
+        setOpacity(1);
+      }, 300);
+      return () => clearTimeout(timer);
+    }
+  }, [card, currentCard]);
   return (
-    <div className='flex items-center transition-opacity duration-500'>
+    <div className='flex items-center transition-opacity duration-1000' style={{opacity}}>
       <div className='flex-col'>
-        <div className="text-[#ea4335] text-[22px] font-semibold font-suite leading-9">{card.subtitle}</div>
-        <h2 className="text-5xl font-bold mt-[12px] relative">{card.title}</h2>
-        <div className='w-[306px] h-[120px] text-[#606060] text-[28px] font-normal font-suite leading-10 mt-[23.12px]'>{card.description}</div>
+        <div className="text-[#ea4335] text-[22px] font-semibold font-suite leading-9">{currentCard.subtitle}</div>
+        <h2 className="text-5xl font-bold mt-[12px] relative">{currentCard.title}</h2>
+        <div className='w-[306px] h-[120px] text-[#606060] text-[28px] font-normal font-suite leading-10 mt-[23.12px]'>{currentCard.description}</div>
       </div>
-      <Image src={card.image} alt={card.title} className="w-[714px] h-[574.12px] relative ml-[102px]" />
+      <Image src={currentCard.image} alt={currentCard.title} className="w-[714px] h-[574.12px] relative ml-[102px]" />
     </div>
   );
 }
