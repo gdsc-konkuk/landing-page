0a3059d76e4f350a5491c63f8f63c85bd2b7c71c
clice lee <cliceleee@gmail.com>
2024-08-10 19:19:05 +0900

    add: big circle color props

--- app/keyword/Dot.tsx
+++ app/keyword/Dot.tsx
@@ -3,16 +3,28 @@ import React from 'react';
 interface DotProps {
   onHover: () => void;
   onLeave: () => void;
-  color: string;
+  smallCiclceColor: string;
+  bigCircleColor: string; //when hover event occurs, each hover color changes
 }
 
-const Dot: React.FC<DotProps> = ({ onHover, onLeave, color }) => {
+const Dot: React.FC<DotProps> = ({ onHover, onLeave, smallCiclceColor, bigCircleColor }) => {
   return (
+    <div className="relative flex items-center justify-center">
+      {/* 큰 원 (뒤쪽) */}
+      <div
+        className={`w-[25px] h-[25px] rounded-full ${smallCiclceColor} transition-colors duration-300`}
+        onMouseEnter={onHover}
+        onMouseLeave={onLeave}
+        style={{
+          backgroundColor: `${smallCiclceColor.replace('700', '300')}`, // ex: red-700 -> red-300
+        }}
+      />
     <div
-      className={`w-[15px] h-[15px] ${color} rounded-full cursor-pointer`}
+      className={`w-[15px] h-[15px] ${smallCiclceColor} rounded-full cursor-pointer`}
       onMouseEnter={onHover}
       onMouseLeave={onLeave}
     />
+    </div>
   );
 }
 
