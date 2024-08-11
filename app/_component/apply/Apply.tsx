b87c1e19e5494d759bcba297f0d35b5c805a6e94
clice lee <cliceleee@gmail.com>
2024-08-11 22:57:59 +0900

    design: adjust text position for mobile view

--- app/_component/apply/Apply.tsx
+++ app/_component/apply/Apply.tsx
@@ -1,3 +1,5 @@
+'use client'
+import { useEffect, useState } from 'react';
 import Popup from "./Popup";
 import ApplyKeywords from "./ApplyKewords";
 import blueCharacter from '../../../public/images/apply/blueCharacter.png';
@@ -5,6 +7,47 @@ import Image from 'next/image';
 import ApplyButton from '../../../components/ui/ApplyButton';
 
 export default function Apply() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768); // 768px is a common breakpoint for mobile devices
+    };
+
+    // Initial check
+    handleResize();
+
+    // Listen for resize events
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup the event listener on component unmount
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  if (isMobile) {
+    return (
+      <div className="flex flex-col items-center text-center mt-[100px]"> 
+        <div className="relative flex flex-col items-center">
+          <div className="w-[128px] absolute -top-[70px] left-1/2 transform -translate-x-1/2 " />
+          <div className="flex flex-col items-center space-y-[11px]">
+            <div className="text-black text-[24px] font-bold">GDSC Konkuk 2기와 </div> 
+            <div className="w-[107px] h-[36px]"><ApplyKeywords /></div>
+          </div>
+        </div>
+       
+        
+        <Popup />
+        <div className="text-black text-xl font-semibold mt-[20px] space-y-[8px]">
+          <p>GDSC와 함께 가치를 공유하고 성장하고 싶다면,</p>
+          <p>2024.08.01.~2025.08.01</p>
+        </div>
+        <div className="mt-[30px]">
+          <ApplyButton />
+          </div>
+
+      </div>
+    );
+  }
   return (
     <div className="flex flex-col items-center text-center mt-[256px]">
       <div className="relative flex flex-col items-center">
