fd6342a1ba174000073f7229c15470f88734c7fa
clice lee <cliceleee@gmail.com>
2024-08-12 16:14:23 +0900

    refactor: Trinary operator was used for mobile view

--- app/_component/apply/Apply.tsx
+++ app/_component/apply/Apply.tsx
@@ -7,7 +7,7 @@ import Image from 'next/image';
 import ApplyButton from '../../../components/ui/ApplyButton';
 
 export default function Apply() {
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 847);
+  const [isMobile, setIsMobile] = useState(typeof window !== 'undefined' && window.innerWidth <= 847);
 
   useEffect(() => {
     const handleResize = () => {
@@ -20,7 +20,7 @@ export default function Apply() {
     };
   }, []);
 
-  return (
+  return isMobile ? (
     <div className="flex flex-col items-center text-center mt-[100px]">
       <div className="relative flex flex-col items-center">
         <div className="w-[128px] absolute -top-[70px] left-1/2 transform -translate-x-1/2 " />
@@ -37,39 +37,29 @@ export default function Apply() {
       </div>
       <div className="mt-[30px]">
         <ApplyButton />
-        </div>
-
-        
-        <Popup />
-        <div className="text-black text-xl font-semibold mt-[20px] space-y-[8px]">
-          <p>GDSC와 함께 가치를 공유하고 성장하고 싶다면,</p>
-          <p>2024.08.01.~2025.08.01</p>
-        </div>
-        <div className="mt-[30px]">
-          <ApplyButton />
-          </div>
-
-      </div>
-    );
-  }
-  return (
-    <div className="flex flex-col items-center text-center mt-[256px]">
-      <div className="relative flex flex-col items-center">
-      <Image src={blueCharacter} alt="Blue Character" 
-      className="w-[128px] absolute -top-[105px] left-1/2 transform translate-x-[150px] z-10"   />
-      <div className="flex items-center space-x-[11px]">
-        <div className=' text-black text-[64px] font-bold'>GDSC Konkuk 2기와 </div> 
-        <div className="z-5"><ApplyKeywords /></div>
       </div>
     </div>
-        <div className='text-black text-3xl font-semibold mt-[40px] space-y-[8px]'>
-            <p>GDSC와 함께 가치를 공유하고 성장하고 싶다면,</p>
-            <p>2024.08.01.~2025.08.01</p>
+  ) : (
+    <div className="flex flex-col items-center text-center mt-[256px]">
+      <div className="relative flex flex-col items-center">
+        <Image 
+          src={blueCharacter} 
+          alt="Blue Character" 
+          className="w-[128px] absolute -top-[105px] left-1/2 transform translate-x-[150px] z-10" 
+        />
+        <div className="flex items-center space-x-[11px]">
+          <div className=' text-black text-[64px] font-bold'>GDSC Konkuk 2기와 </div> 
+          <div className="z-5"><ApplyKeywords isMobile={isMobile} /></div>
         </div>
-        <div className="mt-[45px]">
-          <ApplyButton />
-        </div>
-          <Popup />
+      </div>
+      <div className='text-black text-3xl font-semibold mt-[40px] space-y-[8px]'>
+        <p>GDSC와 함께 가치를 공유하고 성장하고 싶다면,</p>
+        <p>2024.08.01.~2025.08.01</p>
+      </div>
+      <div className="mt-[45px]">
+        <ApplyButton />
+      </div>
+      <Popup />
     </div>
   );
 }
