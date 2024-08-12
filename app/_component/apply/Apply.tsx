9ccdb2c23487fda7ec8c81a202a0d4357fe98dfa
clice lee <cliceleee@gmail.com>
2024-08-12 17:09:43 +0900

    fix: resolve auto line break error -with whitespace-nowrap

--- app/_component/apply/Apply.tsx
+++ app/_component/apply/Apply.tsx
@@ -20,7 +20,7 @@ export default function Apply() {
     };
   }, []);
 
-  return isMobile ? (
+  return isMobile ? ( //mobile view
     <div className="flex flex-col items-center text-center mt-[100px]">
       <div className="relative flex flex-col items-center">
         <div className="w-[128px] absolute -top-[70px] left-1/2 transform -translate-x-1/2 " />
@@ -35,10 +35,10 @@ export default function Apply() {
         <p>2024.08.01.~2025.08.01</p>
       </div>
       <div className="flex items-center justify-center">
-        <ApplyButton className="mt-[23px] md:mt-[56px] w-[128px] h-[37px]" />
+        <ApplyButton className="mt-[23px] md:mt-[56px] w-[128px] h-[37px] whitespace-nowrap" />
       </div>    
     </div>
-  ) : (
+  ) : ( //desktop view
     <div className="flex flex-col items-center text-center mt-[256px]">
       <div className="relative flex flex-col items-center">
         <Image 
@@ -55,7 +55,7 @@ export default function Apply() {
         <p>GDSC와 함께 가치를 공유하고 성장하고 싶다면,</p>
         <p>2024.08.01.~2025.08.01</p>
       </div>
-      <ApplyButton className="mt-[23px] md:mt-[56px] w-[128px] h-[37px]" />
+      <ApplyButton className="mt-[23px] md:mt-[56px] w-[128px] h-[37px] whitespace-nowrap " />
       <Popup />
     </div>
   );
