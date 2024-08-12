f6eb5ed7b426855bed6c9cc417329dd5e1bfcf01
clice lee <cliceleee@gmail.com>
2024-08-12 16:12:29 +0900

    feat: responsive apply component for mobile view

--- app/_component/apply/ApplyKewords.tsx
+++ app/_component/apply/ApplyKewords.tsx
@@ -1,13 +1,31 @@
-export default function ApplyKeywords() {
+export default function ApplyKeywords({ isMobile }) {
   return (
-    <div className="relative h-[80px] w-[236px] overflow-hidden px-5 py-[6px] text-[50px] font-bold">
-      <div className="absolute inset-0 flex h-full w-full animate-slide-4 items-center justify-center rounded-[24px] bg-[#4285F4]">
+    <div
+      className={`relative overflow-hidden font-bold ${
+        isMobile
+          ? 'h-[36px] w-[107px] px-[2px] py-[1px] text-[18px]'
+          : 'h-[80px] w-[236px] px-5 py-[6px] text-[50px]'
+      }`}
+    >
+      <div
+        className={`absolute inset-0 flex h-full w-full items-center justify-center ${
+          isMobile ? 'rounded-[12px]' : 'rounded-[24px]'
+        } bg-[#4285F4]`}
+      >
         도전하기
       </div>
-      <div className="absolute inset-0 flex h-full w-full animate-slide-2 items-center justify-center rounded-[24px] bg-[#34A853]">
+      <div
+        className={`absolute inset-0 flex h-full w-full items-center justify-center ${
+          isMobile ? 'rounded-[12px]' : 'rounded-[24px]'
+        } bg-[#34A853]`}
+      >
         함께하기
       </div>
-      <div className="absolute inset-0 flex h-full w-full animate-slide-0 items-center justify-center rounded-[24px] bg-[#FBBC04]">
+      <div
+        className={`absolute inset-0 flex h-full w-full items-center justify-center ${
+          isMobile ? 'rounded-[12px]' : 'rounded-[24px]'
+        } bg-[#FBBC04]`}
+      >
         성장하기
       </div>
     </div>
