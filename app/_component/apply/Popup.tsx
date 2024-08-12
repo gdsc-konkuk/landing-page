005d7fa6178092e33b1bb6d6341dd0ac94181f89
clice lee <cliceleee@gmail.com>
2024-08-12 16:58:05 +0900

    design: import group card image  for mobile view

--- app/_component/apply/Popup.tsx
+++ app/_component/apply/Popup.tsx
@@ -4,13 +4,22 @@ import Image from 'next/image';
 import pmCard from '../../../public/images/apply/pmCard.png';
 import designerCard from '../../../public/images/apply/designerCard.png';
 import developerCard from '../../../public/images/apply/developerCard.png';
+import mobileCard from '../../../public/images/apply/mobileCard.png';
 import 'animate.css'; //bounce animations library
 
 export default function Popup() {
+  const [isMobile, setIsMobile] = useState(false);
   const [animate, setAnimate] = useState(false);
   const popupRef = useRef(null);
 
   useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth <= 847); // Adjust this breakpoint as needed
+    };
+
+    checkMobile();
+    window.addEventListener('resize', checkMobile);
+
     const observer = new IntersectionObserver(
       (entries) => {
         entries.forEach(entry => {
@@ -29,20 +38,33 @@ export default function Popup() {
     }
 
     return () => {
+      window.removeEventListener('resize', checkMobile);
       if (popupRef.current) {
         observer.unobserve(popupRef.current);
       }
     };
   }, []);
 
+  if (isMobile) {
+    return (
+      <div className="flex justify-center h-screen w-screen w-[382.05px] h-[278.69px] relative">
+        <Image
+          src={mobileCard}
+          alt="Mobile Card"
+          className=""
+        />
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-col items-center justify-center text-center" ref={popupRef}>
       <div
         className={`w-[1380px] h-[700px] overflow-hidden relative ${animate ? 'animate__animated animate__bounceInUp' : 'translate-y-[1000px]'}`}
         style={{
-          animationDuration: '1s', // 애니메이션 속도 설정
-          animationTimingFunction: 'ease-in-out', // 애니메이션 속도 곡선 설정
-          animationDelay: '0.1s', // 애니메이션 지연 시간 설정
+          animationDuration: '1s',
+          animationTimingFunction: 'ease-in-out',
+          animationDelay: '0.1s',
         }}
       >
         <Image
