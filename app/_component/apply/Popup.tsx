fe6fdbb5827d926fc35c42fefb78b297aaec3de1
clice lee <cliceleee@gmail.com>
2024-08-13 23:49:46 +0900

    fix: Apply section popup animation overflow issue -used overflow-hidden

--- app/_component/apply/Popup.tsx
+++ app/_component/apply/Popup.tsx
@@ -58,7 +58,7 @@ export default function Popup() {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center text-center" ref={popupRef}>
+    <div className="flex flex-col items-center justify-center text-center overflow-hidden" ref={popupRef}>
       <div
         className={`w-[1380px] h-[700px] overflow-hidden relative ${animate ? 'animate__animated animate__bounceInUp' : 'translate-y-[1000px]'}`}
         style={{
